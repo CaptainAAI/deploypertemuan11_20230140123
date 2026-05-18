ebaca6d263e798708e27e7b19f2402828a5ff616
Ananda Agung Ismail <anandaagungismail@gmail.com>
2026-05-18 14:47:38 +0700

    Home Controller API Route

--- src/main/java/com/deploy/pertemuan11/controller/HomeController.java
+++ src/main/java/com/deploy/pertemuan11/controller/HomeController.java
@@ -1,4 +1,26 @@
 package com.deploy.pertemuan11.controller;
 
+import ch.qos.logback.core.model.Model;
+import com.deploy.pertemuan11.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final AuthService authService;
+
+    @GetMapping("/home")
+    public String home(Model model) {
+
+        User user = authService.getLoggedInUser();
+
+        model.addAttribute("user", user);
+        model.addAttribute("profile", user.getProfile());
+
+        return "home";
+    }
 }
